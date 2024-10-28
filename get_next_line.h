ec7ddc9540b5e62749fc4e38ad07d03715539451
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-10-28 10:05:58 -0300

    feat: add get_next_line.h with ft_strlen prototype and standard includes

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 10:04:45 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/10/28 10:05:17 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+
+#endif
