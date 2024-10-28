d5238fb91d10b26da602c6c467298c94a0805ff3
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-10-28 10:09:39 -0300

    feat: update get_next_line.h with ft_memcpy and ft_strjoin prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:04:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/28 10:05:17 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/28 10:09:05 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 # include <unistd.h>
 
 size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
