4239d0bec8188a64c36e788393cc2529633b2154
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-10-28 10:20:26 -0300

    feat: implement get_next_line function with BUFFER_SIZE support

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:11:09 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/28 10:12:05 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/28 10:19:42 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,31 @@ static char	*read_line(int fd, char *buffer, char *line)
 {
 	int	bytes_read;
 
-	bytes_read = read(fd, buffer, 2048);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	while (bytes_read > 0)
 	{
 		buffer[bytes_read] = '\0';
 		line = ft_strjoin(line, buffer);
 		if (ft_strchr(line, '\n'))
 			break ;
-		bytes_read = read(fd, buffer, 2048);
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = NULL;
+	line = read_line(fd, buffer, line);
+	if (!line || (line[0] == '\0' && buffer[0] == '\0'))
+	{
+		free(line);
+		return (NULL);
 	}
 	return (line);
 }
