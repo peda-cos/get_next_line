f89ed109fb18909cdaa81efdacbcf0e459c5eeb9
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-10-29 03:38:04 -0300

    refactor: enhance buffer handling in get_next_line with load and clear functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,38 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:11:09 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/29 02:50:25 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/29 03:36:55 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static int	ft_load_line_from_buffer(char **line, char **buffers)
+{
+	int	i;
+
+	i = 0;
+	while (i < BUFFER_SIZE && buffers[0][i])
+	{
+		if (buffers[0][i] == '\n')
+		{
+			*line = ft_strjoin(*line, buffers[0]);
+			ft_memmove(buffers[0], buffers[0] + i + 1, BUFFER_SIZE - (i + 1));
+			ft_memset(buffers[0] + BUFFER_SIZE - (i + 1), 0, i + 1);
+			return (-1);
+		}
+		i++;
+	}
+	return (i);
+}
+
+static char	*ft_clear_buffer(char *line, char **buffers)
+{
+	if (buffers[0])
+		free(buffers[0]);
+	buffers[0] = NULL;
+	return (line);
+}
 
 static char	*ft_read_line_fd(char *line, char **buffers, int fd)
 {
@@ -32,15 +59,15 @@ static char	*ft_read_line_fd(char *line, char **buffers, int fd)
 
 char	*get_next_line(int fd)
 {
-	static char	*buffers[FT_GNL_MAX];
+	static char	*buffers[BUFFER_SIZE];
 
 	if ((fd < 0) || (BUFFER_SIZE < 1))
 		return (NULL);
 	if (read(fd, NULL, 0) < 0)
-		return (ft_clear_buffer(NULL, buffers + fd * (FT_GNL_MAX > 1)));
-	if (buffers[fd * (FT_GNL_MAX > 1)] == NULL)
-		buffers[fd * (FT_GNL_MAX > 1)] = ft_calloc(BUFFER_SIZE, sizeof(char));
-	if (buffers[fd * (FT_GNL_MAX > 1)] == NULL)
+		return (ft_clear_buffer(NULL, buffers + fd * (BUFFER_SIZE > 1)));
+	if (buffers[fd * (BUFFER_SIZE > 1)] == NULL)
+		buffers[fd * (BUFFER_SIZE > 1)] = ft_calloc(BUFFER_SIZE, sizeof(char));
+	if (buffers[fd * (BUFFER_SIZE > 1)] == NULL)
 		return (NULL);
-	return (ft_read_line_fd(NULL, buffers + fd * (FT_GNL_MAX > 1), fd));
+	return (ft_read_line_fd(NULL, buffers + fd * (BUFFER_SIZE > 1), fd));
 }
