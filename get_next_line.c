f9678296fd8dfe4bd3ceeec48babdc136d2c4ab1
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-10-30 03:02:04 -0300

    refactor: restructure get_next_line for clarity and efficiency]

--- get_next_line.c
+++ get_next_line.c
@@ -6,91 +6,101 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 10:53:04 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/30 01:50:01 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/30 02:54:02 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <unistd.h>
 
-static char	*ft_read_buffer(int fd, char *buffer)
+static char	*update_alloc(char *alloc)
 {
-	char	*temp_buffer;
-	ssize_t	bytes_read;
-
-	temp_buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!temp_buffer)
-		return (NULL);
-	bytes_read = 1;
-	while ((!buffer || !ft_strchr(buffer, '\n')) && bytes_read != 0)
-	{
-		buffer = ft_read_and_join(fd, buffer, temp_buffer);
-		if (!buffer)
-			break ;
-	}
-	free(temp_buffer);
-	return (buffer);
-}
-
-static char	*ft_read_and_join(int fd, char *buffer, char *temp_buffer)
-{
-	ssize_t	bytes_read;
-
-	bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
-	if (bytes_read == -1)
-	{
-		free(temp_buffer);
-		free(buffer);
-		return (NULL);
-	}
-	temp_buffer[bytes_read] = '\0';
-	buffer = ft_strjoin(buffer, temp_buffer);
-	return (buffer);
-}
-
-static char	*ft_update_buffer(char *buffer)
-{
-	size_t	i;
-	size_t	j;
-	char	*new_buffer;
-	size_t	buffer_len;
+	int		i;
+	int		j;
+	char	*new_alloc;
 
 	i = 0;
-	while (buffer[i] && buffer[i] != '\n')
+	while (alloc[i] && alloc[i] != '\n')
 		i++;
-	if (!buffer[i])
+	if (!alloc[i])
 	{
-		free(buffer);
+		free(alloc);
 		return (NULL);
 	}
-	buffer_len = ft_strlen(buffer);
-	new_buffer = malloc((buffer_len - i + 1) * sizeof(char));
-	if (!new_buffer)
+	new_alloc = (char *)malloc(sizeof(char) * (ft_strlen(alloc) - i + 1));
+	if (!new_alloc)
 		return (NULL);
 	i++;
 	j = 0;
-	while (buffer[i])
-		new_buffer[j++] = buffer[i++];
-	new_buffer[j] = '\0';
+	while (alloc[i])
+		new_alloc[j++] = alloc[i++];
+	new_alloc[j] = '\0';
+	free(alloc);
+	return (new_alloc);
+}
+
+static char	*extract_line(char *alloc)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!alloc[i])
+		return (NULL);
+	while (alloc[i] && alloc[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (alloc[i] && alloc[i] != '\n')
+	{
+		line[i] = alloc[i];
+		i++;
+	}
+	if (alloc[i] == '\n')
+	{
+		line[i] = alloc[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*read_and_alloc(int fd, char *alloc)
+{
+	ssize_t	read_bytes;
+	char	*buffer;
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(alloc, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[read_bytes] = '\0';
+		alloc = ft_strjoin(alloc, buffer);
+	}
 	free(buffer);
-	return (new_buffer);
+	return (alloc);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
 	char		*line;
+	static char	*alloc;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-	{
-		free(buffer);
-		buffer = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) == -1)
 		return (NULL);
-	}
-	buffer = ft_read_buffer(fd, buffer);
-	if (!buffer)
+	alloc = read_and_alloc(fd, alloc);
+	if (!alloc)
 		return (NULL);
-	line = ft_extract_line(buffer);
-	buffer = ft_update_buffer(buffer);
+	line = extract_line(alloc);
+	alloc = update_alloc(alloc);
 	return (line);
 }
