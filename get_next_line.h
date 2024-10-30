0ecc2da1a3f595684eb5c1e4de4c3d4352f5e875
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-10-30 03:03:11 -0300

    style: organize includes and function prototypes in get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,24 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 10:52:36 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/30 01:27:56 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/30 02:54:43 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
-# include <stdlib.h>
-
-char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_extract_line(char *buffer);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
+char	*ft_strchr(const char *str, int ch);
+char	*get_next_line(int fd);
 
 #endif
