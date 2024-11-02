30545dde2a326128e780379a8ae700ff299fd08e
Pedro Da Costa E Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-02 16:54:36 -0300

    feat: add test files arquivo.txt and main.c

---
+++ main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 16:35:53 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/11/02 16:36:14 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc < 2)
+		fd = 0;
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+		{
+			perror("Erro ao abrir o arquivo");
+			return (1);
+		}
+	}
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	free(line);
+	if (fd != 0)
+		close(fd);
+	return (0);
+}
