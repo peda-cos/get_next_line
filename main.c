1f3d503f9842ff2b52cb5a5dd76013e689571a2d
Pedro Da Costa E Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-02 17:06:18 -0300

    refactor: simplify main.c test loop for get_next_line

--- main.c
+++ main.c
@@ -23,14 +23,7 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		fd = 0;
 	else
-	{
 		fd = open(argv[1], O_RDONLY);
-		if (fd < 0)
-		{
-			perror("Erro ao abrir o arquivo");
-			return (1);
-		}
-	}
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
