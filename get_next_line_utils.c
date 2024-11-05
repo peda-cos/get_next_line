8ca8a2f19884b61372ec1b95f93ef56e3ff6381f
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-05 12:25:33 -0300

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 03:49:23 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/11/02 15:49:14 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/11/05 12:25:14 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 size_t	ft_strlen(const char *str)
 {
-	size_t	i;
+	size_t	len;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
 }
 
 char	*ft_strchr(const char *str, int ch)
@@ -31,7 +31,7 @@ char	*ft_strchr(const char *str, int ch)
 		return (0);
 	if (ch == '\0')
 		return ((char *)&str[ft_strlen(str)]);
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (str[i] == (char)ch)
 			return ((char *)&str[i]);
