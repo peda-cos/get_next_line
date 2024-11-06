929726635958756c55375b39ed1adf451c821f87
Pedro Da Costa E Silva Monteiro <peda-cos@c3r6p4.42sp.org.br>
2024-11-05 21:50:11 -0300

    style: refactor indentation in get_next_line_utils.c for consistency

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -24,44 +24,44 @@ size_t	ft_strlen(const char *str)
 
 char	*ft_strchr(const char *str, int ch)
 {
-    if (!str)
-        return (NULL);
-    if (ch == '\0')
-        return ((char *)&str[ft_strlen(str)]);
-    while (*str)
-    {
-        if (*str == (char)ch)
-            return ((char *)str);
-        str++;
-    }
-    return (NULL);
+	if (!str)
+		return (NULL);
+	if (ch == '\0')
+		return ((char *)&str[ft_strlen(str)]);
+	while (*str)
+	{
+		if (*str == (char)ch)
+			return ((char *)str);
+		str++;
+	}
+	return (NULL);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
 {
-    size_t	i;
-    size_t	j;
-    char	*str;
+	size_t	i;
+	size_t	j;
+	char	*str;
 
-    if (!s1)
-    {
-        s1 = (char *)malloc(1);
-        if (!s1)
-            return (NULL);
-        s1[0] = '\0';
-    }
-    if (!s2)
-        return (NULL);
-    str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-    if (!str)
-        return (NULL);
-    i = -1;
-    while (++i < ft_strlen(s1))
-        str[i] = s1[i];
-    j = 0;
-    while (j < ft_strlen(s2))
-        str[i++] = s2[j++];
-    str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-    free(s1);
-    return (str);
+	if (!s1)
+	{
+		s1 = (char *)malloc(1);
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	if (!s2)
+		return (NULL);
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (++i < ft_strlen(s1))
+	str[i] = s1[i];
+	j = 0;
+	while (j < ft_strlen(s2))
+		str[i++] = s2[j++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
 }
