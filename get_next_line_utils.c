be0369332a25542c3b6b2514f3ca0bb5d7f23c3d
Pedro Da Costa E Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-12 01:50:50 -0300

    refactor: move  to

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -59,3 +59,13 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		*lst = temp;
 	}
 }
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
