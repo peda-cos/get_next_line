9b08723072e1e714bf904f212bc6e5e17da8885c
Pedro Da Costa E Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-12 06:36:11 -0300

    feat: add ft_strlen prototype to header file

--- get_next_line.h
+++ get_next_line.h
@@ -31,5 +31,6 @@ t_list	*ft_lstnew(char *content);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+size_t	ft_strlen(const char *str);
 
 #endif
