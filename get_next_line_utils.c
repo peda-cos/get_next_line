e3a7057bcc76e84a2939e83e44a808cd6ce02adb
Pedro Da Costa E Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-12 07:37:59 -0300

    fix: add null checks in ft_lstclear to prevent potential crashes

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -54,7 +54,9 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*temp;
 
-	while (lst && *lst)
+	if (!lst || !del)
+		return ;
+	while (*lst)
 	{
 		temp = (*lst)->next;
 		ft_lstdelone(*lst, del);
