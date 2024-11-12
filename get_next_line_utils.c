7a0d992f6aae90c827d80d4f985404ff31054e9a
Pedro Da Costa E Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-12 01:38:20 -0300

    feat: add ft_lstadd_back function for appending to linked list in get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -23,3 +23,18 @@ t_list	*ft_lstnew(char *content)
 	node->next = NULL;
 	return (node);
 }
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = *lst;
+	while (last->next)
+		last = last->next;
+	last->next = new;
+}
