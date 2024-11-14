40df15be96e62f16350d4aad52b11b90859bd9c4
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-14 16:37:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 16:36:19 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/11/14 16:37:20 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
-    int fd;
-    char *line;
+	int fd;
+	char *line;
     int i = 1;
 
     while (i < argc)
